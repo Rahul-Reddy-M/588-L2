00e31fd5b2dcd6f5823e2fd1fe6b91374a36a485
rahul <20B81A0588@cvr.ac.in>
2023-02-01 11:24:53 +0530

    updated js

--- ES.js
+++ ES.js
@@ -24,5 +24,60 @@ let a =[1,2,3,4,5,6,7]
 console.log(a)
 console.log(...a)
 
+
+
+
+//` operator printing
 console.log("object")
-let name="rahul"
+let name="cvr";
+let age=20;
+let obj = {
+    name,
+    age,
+    display:function(){
+        return `Name: $(this.name)
+        and
+        Age: $(this.age)`
+    }
+}
+
+console.log(obj.display());
+
+//setTimeout function
+let obj1 = {
+    name,
+    age,
+    display(){
+        console.log(this)
+        setTimeout(function(){
+            console.log("Inside timeout function",this)
+        },2000);    
+    }
+}
+console.log(obj1.display());
+//the above way is asynchronous so the second this refers to window object 
+//the solution is to use bind method shown as follows
+let obj2 = {
+    name,
+    age,
+    display(){
+        console.log(this)
+        setTimeout(function(){
+            console.log("Inside timeout function",this)
+        }.bind(this),2000);    
+    }
+}
+console.log(obj2.display());
+//or we can use a arrow function instead of using the bind as follows
+
+let obj3 = {
+    name,
+    age,
+    display(){
+        console.log(this)
+        setTimeout(()=>{
+            console.log("Inside timeout function",this)
+        },2000);    
+    }
+}
+console.log(obj3.display());
